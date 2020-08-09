a165ede395bc0b71e3a37dcfb91df25bea831f57
NilE <nilenon@gmail.com>
2020-08-09 22:50:07 +0700

    [Edit] isCompleted Form test

--- src/utilities/check/isCompletedForm.test.js
+++ src/utilities/check/isCompletedForm.test.js
@@ -1,41 +1,41 @@
 import { isCompletedForm } from "./isCompletedForm"
 
 const emptyField = {
-    name : "",
-    surname : ""
+  name: "",
+  surname: ""
 }
 
 const notEmptyField = {
-    name : "a",
-    surname : "a",
-    email : "a",
-    id : "a",
-    telNo : "a",
-    advisor : "a",
-    purpose : "a"
+  name: "a",
+  surname: "a",
+  email: "a",
+  id: "a",
+  telNo: "a",
+  advisor: "a",
+  purpose: "a"
 }
 
 describe("isCompletedForm.js", () => {
-   describe("isCompleteForm", () => {
+  describe("isCompleteForm", () => {
     describe("when step = 0 and cart.length > 0", () => {
-       it("should return true" , () => {
-           expect(isCompletedForm(0, {}, ["1", "2"] )).toEqual(true)
-       })
+      it("should return true", () => {
+        expect(isCompletedForm(0, {}, ["1", "2"])).toEqual(true)
+      })
     })
     describe("when step = 0 and cart.length <= 0", () => {
-        it("should return false" , () => {
-            expect(isCompletedForm(0, {}, [] )).toEqual(false)
-        })
-     })
-     describe("when step != 0 and has enter field", () => {
-        it("should return false" , () => {
-            expect(isCompletedForm(0, notEmptyField, [] )).toEqual(false)
-        })
-     })
-     describe("when step != 0 and has not enter field", () => {
-        it("should return true" , () => {
-            expect(isCompletedForm(0, emptyField, [] )).toEqual(false)
-        })
-     })
-   })
+      it("should return false", () => {
+        expect(isCompletedForm(0, {}, [])).toEqual(false)
+      })
+    })
+    describe("when step != 0 and has enter field", () => {
+      it("should return true", () => {
+        expect(isCompletedForm(1, notEmptyField, [])).toEqual(true)
+      })
+    })
+    describe("when step != 0 and has not enter field", () => {
+      it("should return false", () => {
+        expect(isCompletedForm(1, emptyField, [])).toEqual(false)
+      })
+    })
+  })
 })
