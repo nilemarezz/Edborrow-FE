4f6dd11e736b533215fececb8641512eedc0744b
NilE <nilenon@gmail.com>
2020-08-14 20:08:08 +0700

    [Edit] mock request detail

--- src/components/ApplicationList/ApplicationDetail.js
+++ src/components/ApplicationList/ApplicationDetail.js
@@ -23,6 +23,7 @@ const FormContent = styled(Grid)`
 class ApplicationDetail extends React.Component {
 
   render() {
+    console.log(this.props.detail.requestDetail ? this.props.detail.requestDetail.Name : "-")
     return (
       < StyledPaper >
         <Container>
@@ -35,25 +36,25 @@ class ApplicationDetail extends React.Component {
           </Typography>
             <FormContent container spacing={3}>
               <Grid item xs={12} sm={4}>
-                <TextField id="id" label="ID" value={'asd'} fullWidth disabled />
+                <TextField id="id" label="ID" value={this.props.detail.requestDetail ? this.props.detail.requestDetail.userId : "-"} fullWidth disabled />
               </Grid>
               <Grid item xs={12} sm={4}>
-                <TextField id="name" label="Name - Surname" value={"Matas Paosriwong"} fullWidth disabled />
+                <TextField id="name" label="Name - Surname" value={this.props.detail.requestDetail ? this.props.detail.requestDetail.Name : "-"} fullWidth disabled />
               </Grid>
               <Grid item xs={12} sm={4}>
-                <TextField id="email" label="Email" value={"nilenon@gmail.com"} fullWidth disabled />
+                <TextField id="email" label="Email" value={this.props.detail.requestDetail ? this.props.detail.requestDetail.email : "-"} fullWidth disabled />
               </Grid>
               <Grid item xs={12} sm={4}>
-                <TextField id="phone" label="Telephone No." value={"0940035373"} fullWidth disabled />
+                <TextField id="phone" label="Telephone No." value={this.props.detail.requestDetail ? this.props.detail.requestDetail.userTelNo : "-"} fullWidth disabled />
               </Grid>
               <Grid item xs={12} sm={4}>
-                <TextField id="advisor" label="Aadvisor" value={"DR. Olarn"} fullWidth disabled />
+                <TextField id="advisor" label="Aadvisor" value={this.props.detail.requestDetail ? this.props.detail.requestDetail.studentAdvisor : "-"} fullWidth disabled />
               </Grid>
               <Grid item xs={12} sm={4}>
-                <TextField id="create" label="Create Date" value={"DR. Olarn"} fullWidth disabled />
+                <TextField id="create" label="Create Date" value={this.props.detail.requestDetail ? RefactorDate(this.props.detail.requestDetail.transactionDate) : "-"} fullWidth disabled />
               </Grid>
               <Grid item xs={12} sm={4}>
-                <TextField id="purpose" label="Purpose" variant="outlined" value={"For Activity and education in wip camp 12"} fullWidth rows={4} multiline disabled />
+                <TextField id="purpose" label="Purpose" variant="outlined" value={this.props.detail.requestDetail ? this.props.detail.requestDetail.borrowPurpose : "-"} fullWidth rows={4} multiline disabled />
               </Grid>
             </FormContent>
 
@@ -64,22 +65,22 @@ class ApplicationDetail extends React.Component {
           </Typography>
           <FormContent container spacing={3}>
             <Grid item xs={12} sm={5}>
-              <TextField id="borrow date" label="Borrow Date" fullWidth disabled />
+              <TextField id="borrow date" label="Borrow Date" value={this.props.detail.requestDetail ? RefactorDate(this.props.detail.requestDetail.borrowDate) : "-"} fullWidth disabled />
             </Grid>
             <span style={{ marginTop: 20 }}> <strong> _ </strong></span>
             <Grid item xs={12} sm={5}>
-              <TextField id="return date" label="Return Date" fullWidth disabled />
+              <TextField id="return date" label="Return Date" value={this.props.detail.requestDetail ? RefactorDate(this.props.detail.requestDetail.returnDate) : "-"} fullWidth disabled />
             </Grid>
           </FormContent>
 
           <Typography variant="h6" component="h6" style={{ marginTop: 30 }}>
-            Advisor Reject Purpose : -
+            Advisor Reject Purpose : {this.props.detail.requestDetail ? this.props.detail.requestDetail.rejectPurpose ? this.props.detail.requestDetail.rejectPurpose : "-" : "-"}
           </Typography>
 
           <Typography variant="h6" component="h6" style={{ marginTop: 30 }}>
             Items :
           </Typography>
-          <ApplicationDetailTable itemList={this.props.detail} />
+          <ApplicationDetailTable itemList={this.props.detail.requestItem} />
         </Container>
       </ StyledPaper>
     )
